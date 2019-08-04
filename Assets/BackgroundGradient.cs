ed0e14d3fc2c07d226ce1a3a633ec9bf983de459
Theo H <Theo@Harkenbusch.com>
2019-08-04 22:51:02 +0200

    add background cavnas with gradient

---
+++ Assets/BackgroundGradient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundGradient : MonoBehaviour
+{
+    
+    public UnityEngine.UI.RawImage img;
+    public Color top;
+    public Color bottom;
+    
+    private Texture2D backgroundTexture ;
+
+    void Awake()
+    {
+        backgroundTexture  = new Texture2D(1, 2);
+        backgroundTexture.wrapMode = TextureWrapMode.Clamp;
+        backgroundTexture.filterMode = FilterMode.Bilinear;
+        SetColor( bottom, top ) ;
+    }
+
+    public void SetColor( Color color1, Color color2 )
+    {
+        backgroundTexture.SetPixels( new Color[] { color1, color2 } );
+        backgroundTexture.Apply();
+        img.texture = backgroundTexture;
+    }
+}
