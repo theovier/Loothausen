84141ead3301e6b19c46073c986886ce8a3443c0
Theo H <Theo@Harkenbusch.com>
2019-08-04 20:40:27 +0200

    add script to handle (animation) flickering of street lamps

---
+++ Assets/FlickeringLight.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour {
+
+
+    public Component lightComponent;
+    public float minTimeBetweenFlickering = 5;
+    public float maxTimeBetweenFlickering = 25;
+    
+    
+    private Animator animator;
+    private RuntimeAnimatorController animController;
+    private float flickeringAnimDuration;
+    private float timeBetweenFlickering;
+    
+    void Start() {
+        animator = lightComponent.GetComponent<Animator>();
+        animController = animator.runtimeAnimatorController;
+        flickeringAnimDuration = getAnimationDuration("street_lamp_flickering");
+        timeBetweenFlickering = RandomFlickeringTime();
+        StartCoroutine(scheduleFlickeringAnimation(timeBetweenFlickering));
+    }
+
+    private float getAnimationDuration(string name) {
+        foreach (var clip in animController.animationClips) {
+            if (clip.name.Equals(name)) {
+                return clip.length;
+            }
+        }
+        return -1;
+    }
+    
+    IEnumerator scheduleFlickeringAnimation(float wait) {
+        yield return new WaitForSeconds(wait);
+        startFlickeringAnimation();
+    }
+
+    private void startFlickeringAnimation() {
+        animator.SetTrigger("flickerTrigger");
+        timeBetweenFlickering = RandomFlickeringTime();
+        StartCoroutine(scheduleFlickeringAnimation(timeBetweenFlickering));
+    }
+
+    private float RandomFlickeringTime() {
+        return Random.Range(flickeringAnimDuration + minTimeBetweenFlickering, maxTimeBetweenFlickering);
+    }
+}
