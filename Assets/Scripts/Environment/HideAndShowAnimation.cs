64988d353c88fa544a33d240e770ff8b80fdd9e5
Theo H <Theo@Harkenbusch.com>
2019-08-06 11:48:32 +0200

    add script to simulate turning on and off lights in houses

---
+++ Assets/Scripts/Environment/HideAndShowAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HideAndShowAnimation : MonoBehaviour {
+
+    public bool hiddenOnStart = true;
+    public float minTimeShown = 8;
+    public float maxTimeShown = 30;
+    public float minTimeHidden = 8;
+    public float maxTimHidden = 30;
+
+    private SpriteRenderer sprite;
+
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+        if (hiddenOnStart) {
+            sprite.enabled = false;
+        }
+        StartCoroutine(EndlessToggleVisiblity());
+    }
+
+    IEnumerator EndlessToggleVisiblity() {
+        while (true) {
+            var wait = sprite.enabled ? RandomTimeHidden() : RandomTimeShown();
+            yield return new WaitForSeconds(wait);
+            toggleVisibility();
+        }
+    }
+
+    private void toggleVisibility() {
+        sprite.enabled = !sprite.enabled;
+    }
+    
+    private float RandomTimeShown() {
+        return Random.Range(minTimeShown, maxTimeShown);
+    }
+    
+    private float RandomTimeHidden() {
+        return Random.Range(minTimeHidden, maxTimHidden);
+    }
+    
+}
