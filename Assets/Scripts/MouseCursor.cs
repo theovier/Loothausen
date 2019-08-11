588a852a8f08b1bb35252faf609c74ff885d69b6
Theo H <Theo@Harkenbusch.com>
2019-08-11 16:35:34 +0200

    bugfix: cursor was not rendered as UI element.

--- Assets/Scripts/MouseCursor.cs
+++ Assets/Scripts/MouseCursor.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MouseCursor : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class MouseCursor : MonoBehaviour {
+    
     void Start() {
         Cursor.visible = false;
     }
     
-    // Update is called once per frame
     void Update() {
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = cursorPos;
+        transform.position = Input.mousePosition;
     }
     
-    void OnApplicationFocus( bool focusStatus )
-    {
+    void OnApplicationFocus( bool focusStatus ) {
         if (focusStatus) {
             Cursor.visible = false;
         }
