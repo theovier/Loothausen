59c9f91e42a8a301506afa5775e3092ccb3f1c36
Theo H <Theo@Harkenbusch.com>
2019-08-12 16:24:06 +0200

    add display text

---
+++ Assets/Scripts/UI/DisplayName.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DisplayName : MonoBehaviour {
+
+    public NameDisplayController controller;
+    public string text;
+    
+    private void OnMouseEnter() {
+        controller.DisplayText(text);
+    }
+
+    private void OnMouseExit() {
+        controller.Hide();
+    }
+    
+}
---
+++ Assets/Scripts/UI/NameDisplayController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NameDisplayController : MonoBehaviour {
+
+    public TextMeshProUGUI textField;
+    public UIFader fader;
+    public float fadeInDuration = 0.1f;
+    public float fadeOutDuration = 0.1f;
+    
+    private bool hidden;
+    
+    private void Start() {
+        gameObject.SetActive(false);
+    }
+    
+    public void DisplayText(string text) {
+        gameObject.SetActive(true);
+        textField.text = text;
+        fader.FadeIn(fadeInDuration);
+        hidden = false;
+    }
+
+    public void Hide() {
+        if (!IsVisible()) return;
+        hidden = true;
+        StartCoroutine(DeactiveAfterFade(fadeOutDuration));
+    }
+    
+    private IEnumerator DeactiveAfterFade(float fadeDuration) {
+        fader.FadeOut(fadeDuration);
+        yield return new WaitForSeconds(fadeDuration);
+        gameObject.SetActive(false);
+    }
+    
+    public bool IsVisible() {
+        return !hidden && gameObject.activeSelf;
+    }
+}
