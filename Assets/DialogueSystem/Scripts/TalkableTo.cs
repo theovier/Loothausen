fb4ce1a240c3d81e73b5d163d38c2edc9eac15f7
Theo H <Theo@Harkenbusch.com>
2019-08-12 16:43:03 +0200

    change method accessor

--- Assets/DialogueSystem/Scripts/TalkableTo.cs
+++ Assets/DialogueSystem/Scripts/TalkableTo.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using Dialogue;
+﻿using Dialogue;
 using UnityEngine;
 
 public class TalkableTo : MonoBehaviour {
     
     public DialogueGraph conversation;
     
-    public void StartConversation() {
+    private void StartConversation() {
         DialogueManager.Instance.StartDialogue(conversation);
     }
 
-    private void OnMouseUp() {
+    private void OnMouseUpAsButton() {
         StartConversation();
     }
 }
