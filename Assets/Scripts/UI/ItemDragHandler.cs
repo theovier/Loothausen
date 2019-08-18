4c609a6e67db5921bdf8d16b7bc18b33be2af5ad
Theo H <Theo@Harkenbusch.com>
2019-08-18 15:27:16 +0200

    add simple drag and drop test features

---
+++ Assets/Scripts/UI/ItemDragHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler {
+
+    public RectTransform icon;
+    
+    public void OnDrag(PointerEventData eventData) {
+        icon.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        icon.localPosition = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/UI/ItemDropHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDropHandler : MonoBehaviour, IDropHandler {
+    
+    public void OnDrop(PointerEventData eventData) {
+        RectTransform inventoryPanel = transform as RectTransform;
+        if (!RectTransformUtility.RectangleContainsScreenPoint(inventoryPanel, Input.mousePosition)) {
+            Debug.Log("item used on something");
+        }
+    }
+}
