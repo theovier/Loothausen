bb78b90ac68ef6f51efedff36f87a3c7fbd093c4
Theo H <Theo@Harkenbusch.com>
2019-08-18 15:04:49 +0200

    add hovering effect on inventory items

---
+++ Assets/Scripts/UI/InventoryItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventoryItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public NameDisplayController tooltip;
+    public string name;
+    public Vector2 scaleOnHover = new Vector2(1.2f, 1.2f);
+    public float scaleDurationOnHover = 0.25f;
+    
+    private RectTransform self;
+
+    private void Start() {
+        self = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        ScaleUp();
+        tooltip.DisplayText(name);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        ScaleDown();
+        tooltip.Hide();
+    }
+
+    private void ScaleUp() {
+        self.DOScale(new Vector3(scaleOnHover.x, scaleOnHover.y, 0), scaleDurationOnHover);
+    }
+
+    private void ScaleDown() {
+        self.DOScale(new Vector3(1, 1, 0), scaleDurationOnHover);
+    }
+}
