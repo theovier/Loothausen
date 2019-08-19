2383efa99d70f4d9fdff13ebae94b4c36a10a047
Theo H <Theo@Harkenbusch.com>
2019-08-19 12:12:01 +0200

    add really basic fade animation to inventory. Requires rework and refactoring

---
+++ Assets/Scripts/UI/ShowOnHover.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShowOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    
+    public RectTransform inventoryBar;
+    public UIFader fader;
+    public float fadeAnimDuration = 0.25f;
+    public float slideAnimationDuration = 0.5f;
+    public float targetPositionY = -125;
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        fader.FadeIn(fadeAnimDuration);
+        inventoryBar.DOAnchorPosY(targetPositionY, slideAnimationDuration);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        fader.FadeOut(fadeAnimDuration);
+        inventoryBar.DOAnchorPosY(targetPositionY + inventoryBar.sizeDelta.y, slideAnimationDuration);
+    }
+}
