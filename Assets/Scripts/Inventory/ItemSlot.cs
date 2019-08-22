6d7eac8cc074d9457af31906954e83c0171a1529
Theo H <Theo@Harkenbusch.com>
2019-08-22 10:01:54 +0200

    seperate scaleOnHover from itemSlot

--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -1,19 +1,13 @@
-﻿using DG.Tweening;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class ItemSlot : CutoutListWrapper<Item>, IPointerEnterHandler, IPointerExitHandler {
     
-    public Vector2 scaleOnHover = new Vector2(1.2f, 1.2f);
-    public float scaleDurationOnHover = 0.25f;
-    
     private Image iconImage;
-    private RectTransform rectTransform;
     private NameDisplayController tooltip;
 
     private void Awake() {
-        rectTransform = GetComponent<RectTransform>();
         tooltip = GameObject.FindGameObjectWithTag("TooltipBox").GetComponent<NameDisplayController>();
         iconImage = GetComponentsInChildren<Image>()[1];
         gameObject.SetActive(false);
@@ -26,11 +20,9 @@ public class ItemSlot : CutoutListWrapper<Item>, IPointerEnterHandler, IPointerE
     
     public void OnPointerEnter(PointerEventData eventData) {
         tooltip.DisplayText(content.itemName);
-        rectTransform.DOScale(new Vector3(scaleOnHover.x, scaleOnHover.y, 0), scaleDurationOnHover).SetAutoKill(false);
     }
 
     public void OnPointerExit(PointerEventData eventData) {
         tooltip.Hide();
-        rectTransform.DOPlayBackwards();
     }
 }
---
+++ Assets/Scripts/UI/ScaleOnHover.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ScaleOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public Vector2 scaleOnHover = new Vector2(1.2f, 1.2f);
+    public float scaleDurationOnHover = 0.25f;
+    private RectTransform rectTransform;
+    
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData) {
+        rectTransform.DOScale(new Vector3(scaleOnHover.x, scaleOnHover.y, 0), scaleDurationOnHover).SetAutoKill(false);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        rectTransform.DOPlayBackwards();
+    }
+}
