c19cd6b4c220bd4bde4627b47e30ac9bc81332e0
Theo H <Theo@Harkenbusch.com>
2019-08-25 16:11:07 +0200

    add simple script to add items to inventory

---
+++ Assets/Scripts/Inventory/Loot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Loot : MonoBehaviour, IPointerClickHandler {
+
+    public List<Item> loot;
+    private bool looted;
+    
+    public void OnPointerClick(PointerEventData eventData) {
+        if (!looted) {
+            //todo: better to access player and from there the inventory, so we can trigger some gathering animation as well.
+            var inventory = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
+            loot.ForEach(inventory.AddItem);
+            looted = true;
+        }
+    }
+}
