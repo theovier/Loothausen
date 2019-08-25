282349ef35817f26a36d11065e19de47f8ea44fc
Theo H <Theo@Harkenbusch.com>
2019-08-25 17:25:44 +0200

    add indicator to new inventory items that disappears when hovering over the items

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -7,4 +7,12 @@ public class Item : ScriptableObject {
     public string itemName;
     public Sprite icon;
     public ItemType type;
+
+    public bool Unseen {
+        get => unseen;
+        set => unseen = value;
+    }
+
+    //used to determine if the player has "seen" (e.g. hovered over) the item in the inventory
+    private bool unseen = true;
 }
--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -5,21 +5,26 @@ using UnityEngine.UI;
 public class ItemSlot : CutoutListWrapper<Item>, IPointerEnterHandler, IPointerExitHandler {
     
     private Image iconImage;
+    private Image seenIndicatorImage;
     private NameDisplayController tooltip;
 
     private void Awake() {
         tooltip = GameObject.FindGameObjectWithTag("TooltipBox").GetComponent<NameDisplayController>();
         iconImage = GetComponentsInChildren<Image>()[1];
+        seenIndicatorImage = GetComponentsInChildren<Image>()[2];
         gameObject.SetActive(false);
     }
     
     protected override void Refresh() {
         iconImage.sprite = content.icon;
+        seenIndicatorImage.enabled = content.Unseen;
         Enable();
     }
     
     public void OnPointerEnter(PointerEventData eventData) {
         tooltip.DisplayText(content.itemName);
+        content.Unseen = false;
+        seenIndicatorImage.enabled = false;
     }
 
     public void OnPointerExit(PointerEventData eventData) {
