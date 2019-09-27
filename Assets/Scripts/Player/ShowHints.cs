827d9e852887188fba28e44d9be647eab3bceeeb
Theo H <Theo@Harkenbusch.com>
2019-09-27 12:08:45 +0200

    add hint stubs to highlight interactables in the scene on button press

---
+++ Assets/Scripts/Player/ShowHints.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ShowHints : MonoBehaviour {
+
+    private GameObject[] hints;
+    
+    private void Start() { 
+        hints = GameObject.FindGameObjectsWithTag("Hint");
+        HideHints();
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("ShowHints")) {
+            HighlightHints();    
+        }
+        else if (Input.GetButtonUp("ShowHints")) {
+            HideHints();
+        }
+    }
+    
+    private void HighlightHints() {
+        foreach (var hint in hints) {
+            hint.SetActive(true);
+        }
+    }
+    
+    private void HideHints() {
+        foreach (var hint in hints) {
+            hint.SetActive(false);
+        }
+    }
+}
