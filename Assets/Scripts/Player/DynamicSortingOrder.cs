97cf7f51c549862bcd3d07270832495f94b74ac7
Theo H <Theo@Harkenbusch.com>
2019-09-30 12:31:23 +0200

    add new sorting layer and script to adjust sorting order based on y pos

---
+++ Assets/Scripts/Player/DynamicSortingOrder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DynamicSortingOrder : MonoBehaviour {
+    
+    private SpriteRenderer[] spriteRenderers;
+    
+    private void Awake() {
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+    
+    private void LateUpdate() {
+        foreach (var o in spriteRenderers) {
+            o.sortingOrder = (int) (transform.position.y * -100f);
+        }
+    }
+}
