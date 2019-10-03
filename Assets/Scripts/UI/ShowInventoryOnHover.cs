ba4c7113853aaffa4a90eda425a2618450805434
Theo H <Theo@Harkenbusch.com>
2019-10-03 22:33:52 +0200

    refactor inventory hide and show

--- Assets/Scripts/UI/ShowInventoryOnHover.cs
+++ Assets/Scripts/UI/ShowInventoryOnHover.cs
@@ -1,47 +1,29 @@
-﻿using DG.Tweening;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ShowInventoryOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
-    public RectTransform inventoryBar;
-    public float fadeAnimDuration = 0.25f;
-    public float slideAnimationDuration = 0.5f;
-    public float targetPositionY = -125;
-
-    private UIFader fader;
+    private ToggleInventory toggleInventory;
     private bool visibilityLock;
-
     private bool cursorInside;
 
     private void Awake() {
-        fader = gameObject.AddComponent<UIFader>();
-        fader.uiElement = inventoryBar.GetComponent<CanvasGroup>();
+        toggleInventory = GetComponent<ToggleInventory>();
     }
 
     public void OnPointerEnter(PointerEventData eventData) {
         cursorInside = true;
         Player.Instance.HasInventoryOpened = true;
-        ShowInventoryBar();
+        toggleInventory.ShowInventoryBar();
     }
 
     public void OnPointerExit(PointerEventData eventData) {
         cursorInside = false;
         Player.Instance.HasInventoryOpened = false;
         if (visibilityLock) return;
-        HideInventoryBar();
-    }
-
-    private void ShowInventoryBar() {
-        fader.FadeIn(fadeAnimDuration);
-        inventoryBar.DOAnchorPosY(targetPositionY, slideAnimationDuration);
+        toggleInventory.HideInventoryBar();
     }
     
-    private void HideInventoryBar() {
-        fader.FadeOut(fadeAnimDuration);
-        inventoryBar.DOAnchorPosY(targetPositionY + inventoryBar.sizeDelta.y, slideAnimationDuration);
-    }
-
     public void Lock() {
         visibilityLock = true;
     }
@@ -49,7 +31,7 @@ public class ShowInventoryOnHover : MonoBehaviour, IPointerEnterHandler, IPointe
     public void Unlock() {
         visibilityLock = false;
         if (!cursorInside) {
-            HideInventoryBar();
+            toggleInventory.HideInventoryBar();
         }
     }
 }
---
+++ Assets/Scripts/UI/ToggleInventory.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class ToggleInventory : MonoBehaviour {
+
+    public RectTransform inventoryBar;
+    public float fadeAnimDuration = 0.25f;
+    public float slideAnimationDuration = 0.5f;
+    public float targetPositionY = -125;
+
+    private UIFader fader;
+
+    private void Awake() {
+        fader = gameObject.AddComponent<UIFader>();
+        fader.uiElement = inventoryBar.GetComponent<CanvasGroup>();
+    }
+
+    public void ShowInventoryBar() {
+        fader.FadeIn(fadeAnimDuration);
+        inventoryBar.DOAnchorPosY(targetPositionY, slideAnimationDuration);
+    }
+    
+    public void HideInventoryBar() {
+        fader.FadeOut(fadeAnimDuration);
+        inventoryBar.DOAnchorPosY(targetPositionY + inventoryBar.sizeDelta.y, slideAnimationDuration);
+    }
+}
