5c44e2fd40aca7f10a089c6b03ffdfabdda58956
Theo H <Theo@Harkenbusch.com>
2019-10-03 21:15:39 +0200

    add elevator

---
+++ Assets/Scripts/Environment/ElevatorDoor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDoor : MonoBehaviour {
+
+    private Animator animator;
+    private bool open;
+    private SpriteRenderer[] doorSpriteRenderer;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        doorSpriteRenderer = GetComponentsInChildren<SpriteRenderer>();
+        Open();
+    }
+
+    private void Open() {
+        open = true;
+        animator.SetBool("open", open);
+    }
+
+    private void Close() {
+        open = false;
+        animator.SetBool("open", open);
+    }
+    
+    private void ToggleDoor() {
+        if (open) {
+            Close();
+        }
+        else {
+            Open();
+        }
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Mouse1)) {
+            ToggleDoor();
+        }
+    }
+}
